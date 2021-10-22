e1c20a46d30950b2a1670227cfb298482e3b77c6
Vipul Kumar Sharma <vipul_sharma86@optum.com>
2021-10-22 17:46:15 +0530

    Reactive variables & blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,29 @@
 <script>
 	export let name;
-	let age = 27;
+	let age = 30;
 
 	const incrementAge= () => {
 		age += 1;
 	};
+
+	$: uppercaseName = name.toUpperCase();
+
+	const changeName = () => {
+		name = 'vipul';
+	};
+
+	$: console.log(name);
+
+	$: if (name === 'vipul') {
+		age = 27;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}, my age is {age}!</h1>
+	<h1>Hello {uppercaseName}, my age is {age}!</h1>
+	
 	<button on:click="{incrementAge}">Change Age</button>
+	<button on:click="{changeName}">Change Name</button>
 </main>
 
 <style>
