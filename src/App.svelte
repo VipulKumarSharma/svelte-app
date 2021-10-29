959b94c3d7ead36553169de9e873aa0f61b15f9b
Vipul Kumar Sharma <vipul_sharma86@optum.com>
2021-10-29 13:54:59 +0530

    Custom ContactCard component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
-	export let name;
+	import ContactCard from "./component/ContactCard.svelte";
+
+	let name = 'Max';
+	let jobTitle = '';
+	let userImage = '';
+	let description = '';
 	let age = 30;
 
 	const incrementAge= () => {
@@ -27,9 +32,19 @@
 	<h1>Hello {uppercaseName}, my age is {age}!</h1>
 	
 	<button on:click="{incrementAge}">Change Age</button>
-	<button on:click="{changeName}">Change Name</button>
+	<!-- <button on:click="{changeName}">Change Name</button> -->
 	<!-- <input type="text" value="{name}" on:input="{inputEvent}"> -->
 	<input type="text" bind:value="{name}">
+	<input type="text" bind:value="{jobTitle}">
+	<input type="text" bind:value="{userImage}">
+	<textarea rows="3" bind:value="{description}"></textarea>
+
+	<ContactCard 
+		userName="{name}"
+		{jobTitle}
+		{userImage}
+		{description}
+	/>
 </main>
 
 <style>
